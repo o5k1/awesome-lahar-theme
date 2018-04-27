5adf3c9e1aa21abe3c7851c1259edba6b9b8c220
Andrea Tombolato <tombolato.andrea@gmail.com>
2018-04-27 14:14:47 +0200

    FIX: [tag template] show attachments + posts

--- functions.php
+++ functions.php
@@ -9,13 +9,13 @@
  * Add scripts to index's head/footer.
  */
 function lahar_enqueue_scripts() {
-	wp_enqueue_style('garamond-font', 'https://fonts.googleapis.com/css?family=EB+Garamond');
-	wp_enqueue_style('lato-font', 'https://fonts.googleapis.com/css?family=Lato:300,400');
-	wp_enqueue_style('dosis-font', 'https://fonts.googleapis.com/css?family=Dosis');
+	wp_enqueue_style( 'garamond-font', 'https://fonts.googleapis.com/css?family=EB+Garamond' );
+	wp_enqueue_style( 'lato-font', 'https://fonts.googleapis.com/css?family=Lato:300,400' );
+	wp_enqueue_style( 'dosis-font', 'https://fonts.googleapis.com/css?family=Dosis' );
 	wp_enqueue_style( 'reset-style', get_template_directory_uri() . '/css/reset.css' );
-	wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.0.10/css/all.css');
+	wp_enqueue_style( 'font-awesome', 'https://use.fontawesome.com/releases/v5.0.10/css/all.css' );
 	wp_enqueue_style( 'custom-style', get_template_directory_uri() . '/css/awesome-lahar.css' );
-	wp_enqueue_script('masonry', 'https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js', ['jquery'], null, true);
+	wp_enqueue_script( 'masonry', 'https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js', [ 'jquery' ], null, true );
 	wp_enqueue_script( 'custom-scripts', get_template_directory_uri() . '/js/awesome-lahar.js', array( 'jquery' ), null, true );
 }
 
@@ -33,7 +33,7 @@ function lahar_theme_setup() {
 
 	register_nav_menus( array(
 		'primary-nav' => __( 'Primary navigation' ),
-		'social-menu'  => __( 'Social links menu' )
+		'social-menu' => __( 'Social links menu' )
 	) );
 
 	/**
@@ -43,6 +43,39 @@ function lahar_theme_setup() {
 }
 
 add_action( 'init', 'lahar_theme_setup' );
+
+function lahar_get_tag_slug( $title ) {
+	return str_replace( ' ', '-', strtolower( $title ) );
+}
+
+function lahar_get_posts_by_tag( $slug ) {
+	return get_posts( array(
+			'posts_per_page' => - 1,
+			'tax_query'      => array(
+				array(
+					'taxonomy' => 'post_tag',
+					'field'    => 'slug',
+					'terms'    => $slug
+				)
+			)
+		)
+	);
+}
+
+function lahar_get_attachments_by_tag( $slug ) {
+	return get_posts( array(
+			'post_type'      => 'attachment',
+			'posts_per_page' => - 1,
+			'tax_query'      => array(
+				array(
+					'taxonomy' => 'post_tag',
+					'field'    => 'slug',
+					'terms'    => $slug
+				)
+			)
+		)
+	);
+}
 
 
 
--- tag.php
+++ tag.php
@@ -1,11 +1,11 @@
 <?php
 $args = array(
-	'posts_per_page' => -1,
+	'posts_per_page' => - 1,
 	'tax_query'      => array(
 		array(
 			'taxonomy' => 'post_tag',
 			'field'    => 'slug',
-			'terms'    => str_replace(' ', '-',strtolower(single_tag_title('', false)))
+			'terms'    => str_replace( ' ', '-', strtolower( single_tag_title( '', false ) ) )
 		)
 	)
 );
@@ -16,48 +16,41 @@ $postslist = get_posts( $args );
 <?php get_header(); ?>
 <div class="body">
 	<?php get_search_form(); ?>
-	<div class="tag-title">
-		<div class="tag-title__name"><?php single_tag_title()?></div>
-		<div class="tag-title__separator">x</div>
-		<div class="tag-title__magazine">lahar magazine</div>
-	</div>
-	<div class="home-grid">
+    <div class="tag-title">
+        <div class="tag-title__name"><?php single_tag_title() ?></div>
+        <div class="tag-title__separator">x</div>
+        <div class="tag-title__magazine">lahar magazine</div>
+    </div>
+    <div class="home-grid">
 		<?php
-		$args = array(
-			'posts_per_page' => -1,
-			'tax_query'      => array(
-				array(
-					'taxonomy' => 'post_tag',
-					'field'    => 'slug',
-					'terms'    => str_replace(' ', '-',strtolower(single_tag_title('', false)))
-				)
-			)
-		);
-
-		$myposts = get_posts( $args );
+		$tagSlug = lahar_get_tag_slug(single_tag_title( '', false ));
+		$articles = lahar_get_posts_by_tag($tagSlug);
+		$images   = lahar_get_attachments_by_tag($tagSlug);
+		$myposts  = array_merge( $articles, $images );
+		shuffle( $myposts );
 		foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
 			<?php if ( $post->post_type === 'attachment' ) { ?>
-				<a href="<?php the_permalink() ?>" class="home-grid__cell">
-					<div class="home-grid__cell-shadow"></div>
-					<div class="home-grid__cell-post">
-						<div class="home-post__content">
-							<img src="<?php echo wp_get_attachment_image_src( $post->id, 'full' )[0] ?>"/>
-						</div>
-					</div>
-				</a>
+                <a href="<?php the_permalink() ?>" class="home-grid__cell">
+                    <div class="home-grid__cell-shadow"></div>
+                    <div class="home-grid__cell-post">
+                        <div class="home-post__content">
+                            <img src="<?php echo wp_get_attachment_image_src( $post->id, 'full' )[0] ?>"/>
+                        </div>
+                    </div>
+                </a>
 			<?php } else { ?>
-				<a href="<?php the_permalink() ?>" class="home-grid__cell">
-					<div class="home-grid__cell-shadow"></div>
-					<div class="home-grid__cell-post">
-						<div class="home-post__content">
-							<div class="home-post__title"><?php the_title(); ?></div>
-							<div class="home-post__excerpt"><?php the_excerpt(); ?></div>
-						</div>
-					</div>
-				</a>
+                <a href="<?php the_permalink() ?>" class="home-grid__cell">
+                    <div class="home-grid__cell-shadow"></div>
+                    <div class="home-grid__cell-post">
+                        <div class="home-post__content">
+                            <div class="home-post__title"><?php the_title(); ?></div>
+                            <div class="home-post__excerpt"><?php the_excerpt(); ?></div>
+                        </div>
+                    </div>
+                </a>
 			<?php } ?>
 		<?php endforeach;
 		wp_reset_postdata(); ?>
-	</div>
+    </div>
 </div>
 <?php get_footer(); ?>
