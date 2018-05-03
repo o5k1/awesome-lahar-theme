60bec5030a18eb8404ffcff38a2502e5451f380c
Andrea Tombolato <tombolato.andrea@gmail.com>
2018-05-03 14:24:13 +0200

    FIX: rubriche's cover were displayed in archive page

--- page-archive.php
+++ page-archive.php
@@ -1,15 +1,17 @@
 <?php get_header(); ?>
-<div class="body">
-	<?php get_search_form(); ?>
-    <ul class="archive-grid">
-		<?php $terms = apply_filters( 'taxonomy-images-get-terms', '' );
-		foreach ( $terms as $term ):?>
-            <li class="archive-grid__item">
-                <a href="<?php print get_category_link($term)?>">
-	                <?php print wp_get_attachment_image( $term->image_id, 'medium' );?>
-                </a>
-            </li>
-		<?php endforeach; ?>
-    </ul>
-</div>
+    <div class="body">
+		<?php get_search_form(); ?>
+        <ul class="archive-grid">
+			<?php $terms = apply_filters( 'taxonomy-images-get-terms', '' );
+			foreach ( $terms as $term ): ?>
+				<?php if ( ! lahar_has_parent_category( 'Rubriche', $term->term_id ) ): ?>
+                    <li class="archive-grid__item">
+                        <a href="<?php print get_category_link( $term ) ?>">
+							<?php print wp_get_attachment_image( $term->image_id, 'medium' ); ?>
+                        </a>
+                    </li>
+				<?php endif; ?>
+			<?php endforeach; ?>
+        </ul>
+    </div>
 <?php get_footer(); ?>
