8a3935b0e9e884037f5e0acd1510275d2fc5c6b8
Andrea Tombolato <tombolato.andrea@gmail.com>
2018-05-07 14:46:02 +0200

    ADD: contatti page template

---
+++ page-contatti.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+    <div class="body">
+		<?php get_search_form(); ?>
+		<?php gmwd_map( 1, 1 ); ?>
+        <h1 class="home-other__title">Contatti</h1>
+        <div class="idea-editors">
+            <div class="contatti-content">
+                <ul id="menu-social-menu" class="menu">
+                    <li id="menu-item-6949"
+                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6949"><a
+                                href="http://facebook.com"><i class="fab fa-facebook-f"></i></a></li>
+                    <li id="menu-item-6950"
+                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6950"><a
+                                href="http://instagram.com"><i class="fab fa-instagram"></i></a></li>
+                    <li id="menu-item-6964"
+                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6964"><a
+                                href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a></li>
+                    <li id="menu-item-6965"
+                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6965"><a
+                                href="https://vimeo.com/"><i class="fab fa-vimeo-v"></i></a></li>
+                </ul>
+                <div class="contact-form">
+                    <?php print do_shortcode('[contact-form-7 id="6966" title="Contatti"]');?>
+                </div>
+                <p class="contatti-footer">
+                    Ti sei abbonato e hai delle domande? Scrivi a
+                    <a href="mailto:spedizioni@laharmagazine.com">spedizioni@laharmagazine.com</a>
+                    che sistemiamo tutto.
+                </p>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
