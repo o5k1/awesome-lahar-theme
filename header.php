91b4bf5e17af42b27f0961f67f0a463dac27688a
Andrea Tombolato <tombolato.andrea@gmail.com>
2018-09-17 10:26:02 +0200

    CHANGE: browser status bar color to black

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Browser status bar's color-->
-    <meta name="theme-color" content="#333">
-    <meta name="apple-mobile-web-app-status-bar-style" content="#333">
+    <meta name="theme-color" content="black">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <title><?php print lahar_get_site_title() ?></title>
 	<?php wp_head(); ?>
 </head>
