cdab6e14f22a08b4ea25c82acf220693824cd5ca
Andrea Tombolato <tombolato.andrea@gmail.com>
2018-09-17 10:23:38 +0200

    ADD: browser status bar color

--- header.php
+++ header.php
@@ -2,6 +2,9 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Browser status bar's color-->
+    <meta name="theme-color" content="#333">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#333">
     <title><?php print lahar_get_site_title() ?></title>
 	<?php wp_head(); ?>
 </head>
