e5540c35f64e8839ecc8c72bbb49f0b9d461d972
Andrea Tombolato <tombolato.andrea@gmail.com>
2018-09-19 23:36:10 +0200

    ADD: lava page

---
+++ page-43-2.php
@@ -0,0 +1,49 @@
+<video autoplay="" muted="" loop="" id="myVideo" style="
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    min-width: 100%;
+    min-height: 100%;
+">
+	<source src="http://localhost/~atombolato/lahar/wp-content/uploads/2018/09/lava.mp4" type="video/mp4">
+</video>
+<div class="content" style="
+    z-index: 2;
+    position: relative;
+    color: white;
+    max-width: 860px;
+    /* text-align: center; */
+    margin: 30px;
+    font-size: 40px;
+    /* background: rgba(0,0,0,0.2); */
+    padding: 0 40px;
+    font-family: 'Lato', sans-serif;
+    font-weight: bold;
+    /* text-transform: uppercase; */
+">
+	<p>
+		Il Lahar si è infilato in ogni anfratto e ora è giunto il momento di distruggere ogni cosa, compreso quanto detto in Lahar #42 – Enigma.
+	</p><p>
+
+		Nel prossimo numero infatti non parleremo di Lahar #44, che potrebbe senza alcun problema essere una scarpa, ma avrà come tema il segno sotto il quale siamo nati.
+	</p><p>
+		Lahar #43 farà fuoco &amp;amp; fiamme passando con un’occhiataccia dal Napalm alle enormi quantità di energia a basso prezzo dei combustibili che hanno permesso la rapida crescita dell’estrazione, della produzione e del consumo di risorse.
+	</p><p>
+		Lahar magazine intende analizzare il fuoco dall’interno mettendo in relazione situazioni emblematiche quali il Nord (Italia), dove la compiutezza nei mezzi legati al consumo di risorse ha raggiunto livelli quasi eccessivi, precisamente il doppio della media nazionale se riferiti al consumo di suolo, e il Sud dove il contrario assume caratteri stilistici come se inconsciamente Bataille avesse avuto ragione nel dire che quando l’urto contro il limite è inevitabile, l’incompiutezza non può che diventare la regola.
+	</p><p>
+		Fuori dal confine della fiamma, però, avviene tutto ciò che spinge l’animo umano a continuare imperterrito ad agire: attorno al fuoco è il luogo della famiglia, della passione e della celebrazione nonché dei diverbi e della marchiatura.
+		E questo non è altro che una maniera per ricordarci ciò che siamo: Lahar, il magazine con il vulcano intorno.
+	</p><p>
+		Per inviarci un contributo invia una mail a <a style="color:white" href="mailto:laharmagazine@gmail.com">laharmagazine@gmail.com</a> con oggetto la parola “Collabora” e in allegato il tuo contributo, entro e non oltre le 23:59 del 31 Ottobre 2018.
+	</p>
+	<h1>ARTICOLI</h1>
+	<p>
+		Limite massimo di 3000 caratteri (spazi inclusi), formato .rtf, carattere Arial dimensione 16. Nel documento va inserito TITOLO, NOME AUTORE e EMAIL AUTORE.
+	</p>
+	<h1>ILLUSTRAZIONI</h1>
+	<p>
+		Per questo numero i colori sono determinati dalla gamma di falsi colori generati da una termocamera come su questo sfondo.
+		Jpeg a 300 dpi o PSD livelli separati.
+		Mandaci un file .zip con la tua ILLUSTRAZIONE piú un documento con TITOLO, NOME AUTORE e EMAIL AUTORE ed eventuale COMMENTO.
+	</p>
+</div>
