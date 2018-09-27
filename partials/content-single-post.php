638ca0d1cafcc7fd3f2e1bae5844114d374fd908
Andrea Tombolato <tombolato.andrea@gmail.com>
2018-09-27 23:12:15 +0200

    FIX: dont't show "di" if no author given for the post

--- partials/content-single-post.php
+++ partials/content-single-post.php
@@ -3,10 +3,12 @@
 		<?php the_title(); ?>
     </div>
     <div class="single-post__author-wrapper">
-        <span>di </span>
-        <a href="<?php echo get_term_link( get_the_tags()[0] ); ?>" class="single-post__author">
-			<?php echo get_the_tags()[0]->name; ?>
-        </a>
+		<?php if ( get_the_tags() ): ?>
+            <span>di </span>
+            <a href="<?php echo get_term_link( get_the_tags()[0] ); ?>" class="single-post__author">
+				<?php echo get_the_tags()[0]->name; ?>
+            </a>
+		<?php endif; ?>
     </div>
     <div class="single-post__content"><?php the_content(); ?></div>
 </div>
