79c6218da745e0b6b129cc271f093458ee20fedd
Andrea Tombolato <tombolato.andrea@gmail.com>
2018-11-24 17:36:24 +0100

    ADD: [home] footer signature

--- footer.php
+++ footer.php
@@ -3,6 +3,9 @@
 	get_template_part( 'partials/newsletter-subscriber');
     wp_nav_menu( array( 'theme_location' => 'social-menu' ) );
     ?>
+    <div class="footer__signature">
+        This site was made with <i class="fas fa-heart"></i> by <a href="https://www.linkedin.com/in/andreatombolato/">Andrea Tombolato</a>
+    </div>
 </div>
 <?php wp_footer()?>
 </body>
