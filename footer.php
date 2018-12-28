1da1f2de540d4c8f9b8d8cf9ac4271fb6edeb807
Andrea Tombolato <tombolato.andrea@gmail.com>
2018-12-28 19:35:08 +0100

    FIX: support-subscriber footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
-<?php get_template_part( 'partials/support-subscriber' ); ?>
 <div class="footer">
+	<?php get_template_part( 'partials/support-subscriber' ); ?>
 	<?php
 	get_template_part( 'partials/newsletter-subscriber' );
 	wp_nav_menu( array( 'theme_location' => 'social-menu' ) );
--- partials/support-subscriber.php
+++ partials/support-subscriber.php
@@ -1,11 +1,13 @@
 <?php
 $accent_color = get_theme_mod( 'accent_color' );
 ?>
-<div class="magazine-subscriber" style="background-color: <?php print $accent_color ?>;color: <?php print $accent_color ?>">
+<div class="magazine-subscriber"
+     style="background-color: <?php print $accent_color ?>;color: <?php print $accent_color ?>">
     <div class="magazine-subscriber__content">
-        <p>
-            <a href="https://store.laharmagazine.com/product/abbonamento-annuale"
-               class="lahar-button lahar-button--white"><span>abbonati</span></a><span class="magazine-subscriber__content-text"> e sostieni la posterzine del vulcano .</span>
-        </p>
+        <div class="magazine-subscriber__content-text">Vuoi ricevere Lahar Magazine a casa tua - o dove preferisci - una
+            settimana dopo la presentazione ufficiale?
+        </div>
+        <a href="https://store.laharmagazine.com/product/abbonamento-annuale"
+           class="lahar-button lahar-button--white"><span>abbonati</span></a>
     </div>
 </div>
